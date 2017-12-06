0034ada88c4a6a0dcbb269250ace831367225143
Michael <michael_lzd@163.com>
2017-12-06 18:14:02 +0800

    Add picture table

--- php/fileUpload.php
+++ php/fileUpload.php
@@ -3,6 +3,7 @@
  * 图片上传
  */
 header('Access-Control-Allow-Origin:*');   // 指定允许其他域名访问
+include "database.php";
 
 function create_token()
 {
@@ -17,6 +18,13 @@ $file = @$_FILES['file'];
 $upload_path = '../upload/';
 if ($file) {
     if (move_uploaded_file($file['tmp_name'], $upload_path . $filename . '.jpg')) {
+
+        // 插入数据库
+        $picture_name = $filename.'.jpg';
+        $query = "insert into picture(picture) values ('{$picture_name}')";
+        error_log($query);
+        $result = $mysqliConn->query($query);
+        $mysqliConn->close();
         $data = [
             'url' => $filename . '.jpg'
         ];
