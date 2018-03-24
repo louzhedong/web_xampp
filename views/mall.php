b1e2f2022a58caebb6391ef1b51f7b05bae43720
Michael <michael_lzd@163.com>
2018-03-24 12:16:22 +0800

    Add mall page

--- views/mall.php
+++ views/mall.php
@@ -130,5 +130,152 @@ $mall_tab = 'index';
     <div class="col-md-1"></div>
   </div>
 </div>
+
+<div class="package-tour">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="package-tour-header">
+        <p class="title">跟团游</p>
+        <p class="subtitle">旅游需要好的选择，全网精选强力推荐</p>
+      </div>
+      <div class="package-tour-body">
+        <ul class="package-tour-list">
+          <li class="package-tour-one">
+            <p class="pics">
+              <img src="../images/mall/package_tour/1.jpeg" alt="">
+            </p>
+            <div class="mask"></div>
+            <div class="tag">跟团游</div>
+            <h3 class="title">[可用券]深圳直飞新加坡+印尼民丹岛5天4晚跟团游（探寻赤道秘境+穿梭红树林+见证黄金沙漠海洋+环游马坡岛+穿越狮城）</h3>
+            <p class="price"><em>999</em>元起</p>
+          </li>
+          <li class="package-tour-item">
+            <p class="pics">
+              <img src="../images/mall/package_tour/2.jpeg" alt="">
+            </p>
+            <div class="infos">
+              <h3 class="title">天津直飞北海道札幌4天3晚跟团游（品尝北海道特色烤肉+成吉思汗烤肉+石狩锅+一晚温泉酒店，北广岛奥特莱斯畅享购物）</h3>
+              <p class="price"><em>2780</em>元起</p>
+            </div>
+          </li>
+          <li class="package-tour-item">
+            <p class="pics">
+              <img src="../images/mall/package_tour/3.jpeg" alt="">
+            </p>
+            <div class="infos">
+              <h3 class="title">北京直飞埃及10-12日文化之旅（埃航直飞/全程国际五星酒店/2晚红海/车载WiFi）</h3>
+              <p class="price"><em>11299</em>元起</p>
+            </div>
+          </li>
+          <li class="package-tour-item">
+            <p class="pics">
+              <img src="../images/mall/package_tour/4.jpeg" alt="">
+            </p>
+            <div class="infos">
+              <h3 class="title">[国庆]洛杉矶佩吉拉斯5天4晚房车跟团游（小团VIP服务+美国营地体验+自营满意度高+可配上海/长沙/重庆往返机票）</h3>
+              <p class="price"><em>6999</em>元起</p>
+            </div>
+          </li>
+          <li class="package-tour-item">
+            <p class="pics">
+              <img src="../images/mall/package_tour/5.jpeg" alt="">
+            </p>
+            <div class="infos">
+              <h3 class="title">北京直飞斯里兰卡7天6晚跟团游（斯航直飞，自营高满意度，一价全含，0自费，双火车体验，3晚五星酒店）</h3>
+              <p class="price"><em>8289</em>元起</p>
+            </div>
+          </li>
+          <li class="package-tour-item">
+            <p class="pics">
+              <img src="../images/mall/package_tour/6.jpeg" alt="">
+            </p>
+            <div class="infos">
+              <h3 class="title">武汉直飞泰国普吉岛+斯米兰岛6天5晚跟团游（安排2晚当五沙滩别墅1晚国际五星2晚国际四星+普吉岛五星SPA+可选择斯米兰群岛浮潜）</h3>
+              <p class="price"><em>3680</em>元起</p>
+            </div>
+          </li>
+          <div class="package-tour-more">
+            <p class="title">查看更多<br>跟团游产品</p>
+            <p class="arrow">
+              <span class="glyphicon glyphicon-menu-right"></span>
+            </p>
+          </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
+<div class="dangdi-tour">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="dangdi-tour-header">
+        <p class="title">当地自助游</p>
+        <p class="subtitle">像当地人一样，体验最本土的吃喝玩乐</p>
+      </div>
+      <div class="dangdi-tour-body">
+        <ul class="dangdi-tour-list">
+          <div class="dangdi-tour-one">
+            <p class="pics">
+              <img src="../images/mall/dangdi/1.jpeg" alt="">
+            </p>
+            <div class="mask"></div>
+            <h3 class="title">【3月22再秒杀】【打卡必玩】日本东京迪斯尼乐园/迪士尼海洋1日/多日通票</h3>
+            <div class="infos">
+              <p class="type">城市玩乐</p>
+              <p class="price"><em>435</em>元起</p>
+            </div>
+          </div>
+          <div class="dangdi-tour-item">
+            <div class="pics">
+              <img src="../images/mall/dangdi/2.jpeg" alt="">
+            </div>
+            <h3 class="title">【樱花季买二免一】【穷游Q-Home首发】日本京都祇园花见和服体验</h3>
+            <p class="price"><em>240</em>元起</p>
+          </div>
+          <div class="dangdi-tour-item">
+            <div class="pics">
+              <img src="../images/mall/dangdi/3.jpeg" alt="">
+            </div>
+            <h3 class="title">【【3月21秒杀】【酒店接】日本大阪京都一日游金阁寺+伏见稻荷大社+祗园等（大阪往返，一人成团）</h3>
+            <p class="price"><em>195</em>元起</p>
+          </div>
+          <div class="dangdi-tour-item">
+            <div class="pics">
+              <img src="../images/mall/dangdi/4.jpeg" alt="">
+            </div>
+            <h3 class="title">【3月22再秒】【打卡必玩】日本大阪环球影城入园电子票</h3>
+            <p class="price"><em>466</em>元起</p>
+          </div>
+          <div class="dangdi-tour-more">
+            <p class="title">查看更多<br>当地玩乐产品</p>
+            <p class="arrow">
+              <span class="glyphicon glyphicon-menu-right"></span>
+            </p>
+          </div>
+          <div class="dangdi-tour-item">
+            <div class="pics">
+              <img src="../images/mall/dangdi/5.jpeg" alt="">
+            </div>
+            <h3 class="title">日本青森县星野酒店集团奥入濑溪流酒店</h3>
+            <p class="price"><em>599</em>元起</p>
+          </div>
+          <div class="dangdi-tour-item">
+            <div class="pics">
+              <img src="../images/mall/dangdi/6.jpeg" alt="">
+            </div>
+            <h3 class="title">日本青森县星野酒店集团青森屋</h3>
+            <p class="price"><em>388</em>元起</p>
+          </div>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
+<?php require 'footer.php' ?>
 </body>
 </html>
