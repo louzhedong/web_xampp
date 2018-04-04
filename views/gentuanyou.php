80c209ab95431f1c673590af943d0f6702ba37de
Michael <michael_lzd@163.com>
2018-04-04 15:10:18 +0800

    Update gentuanyou

--- views/gentuanyou.php
+++ views/gentuanyou.php
@@ -6,7 +6,8 @@
 <html lang="zh-CN">
 <head>
   <title>跟团游</title>
-  <?php require 'link.php' ?>
+    <?php require 'link.php' ?>
+  <link rel="stylesheet" href="../css/gentuanyou.css">
 </head>
 <body>
 <?php
@@ -14,9 +15,152 @@ $tab = 'mall';
 $mall_tab = 'gentuanyou';
 ?>
 <?php require 'top.php' ?>
-<?php require 'mall_top.php'?>
-<div>
-  跟团游
+<?php require 'mall_top.php' ?>
+<div class="gentuanyou">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="gentuanyou-banner">
+        <div class="top">
+          <img src="../images/mall/gentuanyou/1.jpg" alt="">
+        </div>
+        <ul class="bottom">
+          <li>
+            <img src="../images/mall/gentuanyou/2.jpg" alt="">
+          </li>
+          <li>
+            <img src="../images/mall/gentuanyou/3.jpg" alt="">
+          </li>
+          <li>
+            <img src="../images/mall/gentuanyou/4.jpg" alt="">
+          </li>
+        </ul>
+      </div>
+      <div class="gentuanyou-recommend">
+        <div class="gentuanyou-recommend-header">
+          <span class="title">优选推荐</span>
+          <span class="subtitle">集合热门目的地玩法与热选商品，从此告别选择困难症</span>
+        </div>
+        <ul class="gentuanyou-recommend-content">
+          <li class="gentuanyou-recommend-menu">
+            <h2 class="title"></h2>
+            <p class="list"><a>听说 你每年都想来一次京都</a><a>请1天假就能游澳门</a><a>去曼谷 有这一篇攻略就够了</a></p>
+            <h4 class="hot_title">热卖推荐</h4>
+            <p class="hot_list"><a>日本东京迪士尼通票</a><span class="gapline"></span><a>在新加坡与动物共进晚餐</a><span
+                class="gapline"></span><a>体验英国古堡风范</a></p>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/1.jpeg" alt="" width="570" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">冰火共存 探索极光国度</h3>
+              <p class="subtitle">冰岛游玩全攻略</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/2.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">今夏南法 不可错过</h3>
+              <p class="subtitle">普罗旺斯及周边玩法大集合</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/3.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">关西漫步</h3>
+              <p class="subtitle">盘点大阪及周边的HOT SPOT</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/4.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">遇见迪拜 邂逅奢华</h3>
+              <p class="subtitle">迪拜及周边必体验 SPOT</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/5.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">最佳时节的美国西岸</h3>
+              <p class="subtitle">玩转洛杉矶 方法任你选 SPOT</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="gentuanyou-recommend">
+        <div class="gentuanyou-recommend-header">
+          <span class="title">玩转全球</span>
+          <span class="subtitle">像当地人一样，体验最本土的吃喝玩乐</span>
+        </div>
+        <ul class="gentuanyou-recommend-content">
+          <li class="gentuanyou-recommend-menu red">
+            <h2 class="title">东南亚 南亚</h2>
+            <p class="list"><a>新加坡</a><a>普吉</a><a>清迈</a><a>苏梅岛</a><a>巴厘岛</a><a>沙巴</a><a>吴哥</a></p>
+            <h4 class="hot_title">热卖推荐</h4>
+            <p class="hot_list"><a>丛林飞跃</a><span class="gapline"></span><a>海岛浮潜</a><span
+                class="gapline"></span><a>圣淘沙</a><span class="gapline"></span><a>新加坡环球影城</a><span
+                class="gapline"></span><a>滨海湾花园</a></p>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/6.jpeg" alt="" width="570" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">与泰国一起 为美食疯狂</h3>
+              <p class="subtitle">曼谷+清迈人气餐厅大盘点</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/7.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">嗨玩清迈</h3>
+              <p class="subtitle">大众小众全cover</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/8.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">情迷巴厘岛</h3>
+              <p class="subtitle">与情人共同漫步夕阳下的海滩</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/9.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">除了雪这里拥有一切</h3>
+              <p class="subtitle">跟随驴享汇 了解斯里兰卡</p>
+            </div>
+          </li>
+          <li class="gentuanyou-recommend-item">
+            <div class="image">
+              <img src="../images/mall/gentuanyou/recommend/10.jpeg" alt="" width="275" height="186">
+            </div>
+            <div class="titles">
+              <h3 class="title">一生要来一次一次定爱一回</h3>
+              <p class="subtitle">柬埔寨吴哥窟之旅</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
 </div>
+<?php require 'footer.php' ?>
 </body>
 </html>
