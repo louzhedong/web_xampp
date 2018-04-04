e2a9c510b6459de41220a4ad259e8e704dda0453
Michael <michael_lzd@163.com>
2018-04-04 14:13:00 +0800

    Update cruise

--- views/cruise.php
+++ views/cruise.php
@@ -5,8 +5,9 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-    <title>邮轮</title>
+  <title>邮轮</title>
     <?php require 'link.php' ?>
+  <link rel="stylesheet" href="../css/cruise.css">
 </head>
 <body>
 <?php
@@ -14,9 +15,379 @@ $tab = 'mall';
 $mall_tab = 'cruise';
 ?>
 <?php require 'top.php' ?>
-<?php require 'mall_top.php'?>
-<div>
-    邮轮
+<?php require 'mall_top.php' ?>
+<div class="cruise">
+  <div class="cruise-banner">
+    <img src="../images/mall/cruise/banner.jpeg" alt="">
+  </div>
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="cruise-recommend">
+        <div class="cruise-recommend-header">
+          <span class="title">
+            优选推荐
+          </span>
+          <span class="subtitle">
+            近期好货，不容错过，经常甩尾，不看后悔
+          </span>
+        </div>
+        <ul class="cruise-recommend-content">
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/1.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  大西洋号
+                </div>
+                <div class="fr">
+                  <em>1525</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[04月10日]歌诗达邮轮大西洋号深圳往返岘港5天4晚邮...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/2.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  海洋量子号
+                </div>
+                <div class="fr">
+                  <em>3299</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[04月14日]皇家加勒比海洋量子号上海往返熊本5天4晚...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/3.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  双子星号
+                </div>
+                <div class="fr">
+                  <em>2258</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[04月28日]丽星邮轮双子星号厦门往返宫古岛4天3晚邮...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/4.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  大西洋号
+                </div>
+                <div class="fr">
+                  <em>1998</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[04月28日]歌诗达邮轮大西洋号深圳往返马尼拉+苏比...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/5.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  大西洋号
+                </div>
+                <div class="fr">
+                  <em>1888</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[05月19日]歌诗达邮轮大西洋号深圳往返马尼拉+苏比...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/6.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  海洋赞礼号
+                </div>
+                <div class="fr">
+                  <em>3098</em>元起
+                </div>
+              </div>
+            </div>
+            <p>【端午航次早鸟预售】[06月15日]皇家加勒比海洋赞礼...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/7.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  双子星号
+                </div>
+                <div class="fr">
+                  <em>2098</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[06月15日]丽星邮轮双子星号厦门往返宫古岛4天3晚邮...</p>
+          </li>
+          <li class="cruise-recommend-item">
+            <div class="image">
+              <img src="../images/mall/cruise/recommend/8.jpeg" alt="">
+              <div class="price">
+                <div class="fl">
+                  海洋量子号
+                </div>
+                <div class="fr">
+                  <em>3499</em>元起
+                </div>
+              </div>
+            </div>
+            <p>[05月08日]皇家加勒比海洋量子号上海往返长崎5天4晚...</p>
+          </li>
+        </ul>
+      </div>
+      <div class="cruise-calendar">
+        <div class="cruise-calendar-header">
+          <span class="title">邮轮日历</>
+          <span class="subtitle">按假期找对船，选择恐惧不犯难</span>
+        </div>
+        <div class="calendar">
+          <div class="calendar-header">
+            <b>出发地</b>
+            <i class="line"></i>
+            <span class="active">全部<i></i></span>
+            <span>上海<i></i></span>
+            <span>天津<i></i></span>
+            <span>广州<i></i></span>
+            <span>北京<i></i></span>
+            <span>深圳<i></i></span>
+            <span>厦门<i></i></span>
+            <span>香港<i></i></span>
+          </div>
+          <div class="calendar-box">
+            <div class="month">
+              <span>2018年<br>4月</span>
+            </div>
+            <ul class="date-tab">
+              <li class="tab-header">
+                星期日
+              </li>
+              <li class="tab-header">
+                星期一
+              </li>
+              <li class="tab-header">
+                星期二
+              </li>
+              <li class="tab-header">
+                星期三
+              </li>
+              <li class="tab-header">
+                星期四
+              </li>
+              <li class="tab-header">
+                星期五
+              </li>
+              <li class="tab-header">
+                星期六
+              </li>
+              <li>
+                <span class="calendar_data">1</span>
+                <span class="price">1679元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">2</span>
+              </li>
+              <li>
+                <span class="calendar_data">3</span>
+                <span class="price">1998元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">4</span>
+                <span class="price">1679元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">5</span>
+                <span class="price">2099元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">6</span>
+                <span class="price">2028元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">7</span>
+                <span class="price">6900元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">8</span>
+                <span class="price">674元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">9</span>
+                <span class="price">2799元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">10</span>
+                <span class="price">2040元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">11</span>
+                <span class="price">3498元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">12</span>
+                <span class="price">3488元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">13</span>
+                <span class="price">2980元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">14</span>
+                <span class="price">7178元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">15</span>
+                <span class="price">1740元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">16</span>
+                <span class="price">8522元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">17</span>
+                <span class="price">3599元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">18</span>
+                <span class="price">2288元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">19</span>
+                <span class="price">1830元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">20</span>
+                <span class="price">1999元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">21</span>
+                <span class="price">4228元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">22</span>
+                <span class="price">3399元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">23</span>
+              </li>
+              <li>
+                <span class="calendar_data">24</span>
+                <span class="price">2999元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">25</span>
+                <span class="price">1628元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">26</span>
+                <span class="price">3668元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">27</span>
+                <span class="price">2299元起</span>
+              </li>
+              <li>
+                <span class="calendar_data">28</span>
+                <span class="price">2480元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">29</span>
+                <span class="price">2399元起</span>
+                <span class="rest_day_icon">休</span>
+              </li>
+              <li>
+                <span class="calendar_data">30</span>
+                <span class="price">2099元起</span>
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="cruise-ship">
+        <div class="cruise-ship-header">
+          <span class="title">邮轮推荐</span>
+          <span class="subtitle">总有一款适合你</span>
+        </div>
+        <ul class="cruise-ship-content">
+          <li class="cruise-ship-item">
+            <img src="../images/mall/cruise/ship/1.jpg" alt="" class="first">
+            <img src="../images/mall/cruise/ship/2.jpg" alt="" class="second">
+            <div class="name">歌诗达邮轮</div>
+            <div class="sub_list">
+              <div class="sub_list_txt">
+                <p><span>。</span>赛琳娜号 </p>
+                <p><span>。</span>大西洋号 </p>
+                <p><span>。</span>幸运号 </p>
+              </div>
+            </div>
+          </li>
+          <li class="cruise-ship-item">
+            <img src="../images/mall/cruise/ship/3.jpg" alt="" class="first">
+            <img src="../images/mall/cruise/ship/4.jpg" alt="" class="second">
+            <div class="name">皇家加勒比邮轮</div>
+            <div class="sub_list">
+              <div class="sub_list_txt">
+                <p><span>。</span>海洋量子号 </p>
+                <p><span>。</span>海洋礼赞号 </p>
+                <p><span>。</span>海洋水手号 </p>
+              </div>
+            </div>
+          </li>
+          <li class="cruise-ship-item">
+            <img src="../images/mall/cruise/ship/5.jpg" alt="" class="first">
+            <img src="../images/mall/cruise/ship/6.jpg" alt="" class="second">
+            <div class="name">诺唯真邮轮</div>
+            <div class="sub_list">
+              <div class="sub_list_txt">
+                <p><span>。</span>喜悦号 </p>
+                <p><span>。</span>爱彼号 </p>
+                <p><span>。</span>畅意号 </p>
+              </div>
+            </div>
+          </li>
+          <li class="cruise-ship-item">
+            <img src="../images/mall/cruise/ship/7.jpg" alt="" class="first">
+            <img src="../images/mall/cruise/ship/8.jpg" alt="" class="second">
+            <div class="name">丽星邮轮</div>
+            <div class="sub_list">
+              <div class="sub_list_txt">
+                <p><span>。</span>处女星号 </p>
+                <p><span>。</span>双鱼星号 </p>
+                <p><span>。</span>宝瓶星号 </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
 </div>
+<?php require 'footer.php' ?>
 </body>
 </html>
